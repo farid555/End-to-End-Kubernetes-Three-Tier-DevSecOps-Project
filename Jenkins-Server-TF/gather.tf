0f7991cbad0f680ea263fc1722bc32ecbbf9b154
farid555 <farid555a@gmail.com>
2024-12-19 23:56:56 +0200

    modify jenkins-server

--- Jenkins-Server-TF/gather.tf
+++ Jenkins-Server-TF/gather.tf
@@ -6,5 +6,11 @@ data "aws_ami" "ami" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
+    filter{
+    name = "architecture"
+    values = ["x86_64"]
+  }
+
+
   owners = ["099720109477"]
 }
--- Jenkins-Server-TF/variables.tf
+++ Jenkins-Server-TF/variables.tf
@@ -1,8 +1,47 @@
-variable "vpc-name" {}
-variable "igw-name" {}
-variable "rt-name" {}
-variable "subnet-name" {}
-variable "sg-name" {}
-variable "instance-name" {}
-variable "key-name" {}
-variable "iam-role" {}
+variable "vpc-name" {
+  description = "VPC Name for Server"
+  type        = string
+  default     = "Jenkins-vpc"
+}
+
+variable "igw-name" {
+  description = "Internet Gate Way"
+  type        = string
+  default     = "Jenkins-igw"
+}
+
+variable "subnet-name" {
+  description = "Subnet for Sever Jenkins Server"
+  type        = string
+  default     = "Jenkins-subnet"
+}
+
+variable "rt-name" {
+  description = "Jenkins Route Table"
+  type        = string
+  default     = "Jenkins-route-table"
+}
+
+variable "sg-name" {
+  description = "Security Group for Jenkins Server"
+  type        = string
+  default     = "Jenkins-sg"
+}
+
+variable "instance-name" {
+  description = "Server Name"
+  type        = string
+  default     = "Jenkins-server"
+}
+
+variable "key-name" {
+  description = "EC2 keypair"
+  type        = string
+  default     = "terraform_newKey"
+}
+
+variable "iam-role" {
+  description = "jenkins Server I-am Role"
+  type        = string
+  default     = "Jenkins-iam-role"
+}
